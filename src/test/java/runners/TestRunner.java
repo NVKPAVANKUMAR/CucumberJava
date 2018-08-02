fe9829b97449f6a4c4e8eb814df96de268b53703
NVKPAVANKUMAR <npavankumar144@gmail.com>
2018-08-02 18:14:33 +0530

    Feature file path modified

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "C:\\Users\\pavan.nemalikanti\\IdeaProjects\\CucumberFramework\\src\\test\\resources\\FunctionalTests",
+        features = "FunctionalTests/End2End_Test.feature",
         glue = {"stepDefinitions"},
         plugin = {"pretty", "html:target/cucumber-reports"},
         monochrome = true)
